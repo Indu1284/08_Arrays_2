4138fec757f1905895e58a0fc28f0f6a360e04c3
Indu <indurevathy@gmail.com>
2021-08-04 11:59:11 +0200

    code added

--- script_01.js
+++ script_01.js
@@ -9,3 +9,36 @@
 
 // Parameter --> funktion satzbau([ ...], "Q"){ ...}
 
+ausgabe(satzbau(["Ich","bin","Max"],"S"));
+ausgabe(satzbau(["Bist","du","Max"],"Q"));
+ausgabe(satzbau(["Ich","bin"],"E"));
+ausgabe(satzbau(["Ich"],"F"));
+function satzbau(param1,param2) {
+    const gap = " ";
+    const punct = ".";
+    const frage = "?";
+    const exclamation = "!";
+    const semicolon = ":";
+    let str ="";
+    for(let i=0 ; i <param1.length ; i++){
+        
+        if(i != param1.length - 1){
+            str += param1[i] + gap;
+        }else {
+            switch (param2) {
+                case "S" : str += param1[i] + punct;
+                break;
+                case "Q" : str += param1[i] + frage;
+                break;
+                case "E" : str += param1[i] + exclamation;
+                break;
+                default : str += param1[i] + semicolon;
+            }
+        }
+}
+    return str;
+}
+
+function ausgabe(outputstr) {
+    console.log(outputstr);
+}
